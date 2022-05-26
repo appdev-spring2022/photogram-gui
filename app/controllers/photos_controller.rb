bf8a9edde2ecf06a4f497883dd607372c50bf44a
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-05-26 00:07:47 +0000

    V5

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,15 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({:created_at => :desc})
     render({ :template => "photo_templates/index.html.erb" })
   end
+
+  def show
+    url_photo = params.fetch("photo_name")
+
+    matching_photo = User.where(:username => url_photo)
+
+    @the_photo = matching_photo.at(0)
+
+    render({ :template => "photo_templates/show.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get("/users/:path_username",{ :controller => "users", :action => "show"})
 
   get("/photos",{ :controller => "photos", :action => "index"})
+  get("/photos/:photo_name",{ :controller => "photos", :action => "show"})
 
   
 end
